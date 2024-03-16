997a2f083a4f4e429976d36fbdf7076ca8bc392e
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-03-17 02:36:50 +0330

    change handlers output to json

--- core/handlers.rs
+++ core/handlers.rs
@@ -1,11 +1,22 @@
-use axum::{response::Html};
+use axum::{http::StatusCode, response::Json};
+use serde_json::json;
 
 
-pub async fn hello_world_handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+pub async fn hello_world_handler()  -> (StatusCode, Json<serde_json::Value>) {
+    let data = json!({
+        "message": "Hello, world!",
+        "status": "success"
+    });
+
+    (StatusCode::OK, Json(data))
 }
 
 
-pub async fn todo_handler() -> Html<&'static str> {
-    Html("<h1>Todo handler</h1>")
+pub async fn todo_handler() -> (StatusCode, Json<serde_json::Value>) {
+    let data = json!({
+        "message": "Todo!.",
+        "status": "success"
+    });
+
+    (StatusCode::OK, Json(data))
 }
